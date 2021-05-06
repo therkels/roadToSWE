3cb252590576413b5b65776d73d107a2590a523b
jakob <therkels@msu.edu>
2021-05-05 22:15:14 -0400

    recursion

--- app/src/RecursionAndDynamic.py
+++ app/src/RecursionAndDynamic.py
@@ -1,2 +1,8 @@
-def fun_function():
-    return "fun"
+def triple_step_recurse(n):
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+    return triple_step_recurse(n-1) + triple_step_recurse(n-2) + triple_step_recurse(n-3)
+
+print(triple_step_recurse(20))
